b77e5ff33a77eb7ba831920e2b11799d6aefc535
JusBrett <brettfortune01@gmail.com>
2022-10-06 15:53:22 +0200

    JS toggle cards added

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert(hello);
+const ratingCard = document.querySelector(".rating-container");
+const thankyouCard = document.querySelector(".thankyou-container");
+const submitButton = document.querySelector(".btn-submit");
+const rating = document.querySelector(".rate");
+
+
+
+
+
+
+// toggle between cards
+submitButton.addEventListener("click", function(){
+    ratingCard.style.display = "none";
+    thankyouCard.classList.remove("hidden");
+
+});
