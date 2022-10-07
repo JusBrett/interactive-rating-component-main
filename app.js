440d04fb61c2f5413fc161257bd76c0578eb457c
JusBrett <brettfortune01@gmail.com>
2022-10-07 15:51:53 +0200

    completed JS

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const ratingCard = document.querySelector(".rating-container");
 const thankyouCard = document.querySelector(".thankyou-container");
 const submitButton = document.querySelector(".btn-submit");
-const rating = document.querySelector(".rate");
-
-
-
-
+const rating = document.querySelector(".rate"); //span
+const rateSelectors = document.querySelectorAll(".btn") //buttons
 
 
 // toggle between cards
@@ -14,3 +11,15 @@ submitButton.addEventListener("click", function(){
     thankyouCard.classList.remove("hidden");
 
 });
+
+// loop through buttons to get innerHTML
+let numberofbuttons = document.querySelectorAll(".btn").length;
+
+for(let i = 0; i < numberofbuttons; i++){
+
+    document.querySelectorAll(".btn")[i].addEventListener("click", function(){
+
+        rating.innerHTML = this.innerHTML;
+    })
+}
+
